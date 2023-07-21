d6ca9716618822f9fd0373bafb482df053978724
Kaushik Biswas <kbutsho@gmail.com>
2023-07-22 00:40:54 +0600

    database changed

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,10 +7,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_LOCAL,
-  default_seller_pass: process.env.DEFAULT_SELLER_PASS,
-  default_buyer_pass: process.env.DEFAULT_BUYER_PASS,
-  default_admin_pass: process.env.DEFAULT_ADMIN_PASS,
+  database_url: process.env.DATABASE,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt: {
     secret: process.env.JWT_SECRET,
