161d719752cde0df5f9db7ea8d103862cab46d25
Kaushik Biswas <kbutsho@gmail.com>
2023-07-23 11:31:10 +0600

    admin signup route added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,7 +1,7 @@
 import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
 import { UserValidation } from '../user/user.validation';
 import { AuthController } from './auth.controller';
-import validateRequest from '../../middleware/validateRequest';
 import { AuthValidation } from './auth.validation';
 
 const router = express.Router();
@@ -9,7 +9,8 @@ const router = express.Router();
 router.post('/signup', validateRequest(UserValidation.createUser), AuthController.signup);
 router.post('/login', validateRequest(AuthValidation.loginZodSchema), AuthController.login);
 router.post('/login/google', validateRequest(AuthValidation.loginWithGoogleZodSchema), AuthController.loginWithGoogle);
-
-
+// admin 
+router.post('/admin/signup', validateRequest(UserValidation.createUser), AuthController.adminSignup);
+router.post('/admin/login/google', validateRequest(AuthValidation.loginWithGoogleZodSchema), AuthController.adminLoginWithGoogle);
 
 export const AuthRoutes = router;
