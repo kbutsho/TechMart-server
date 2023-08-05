30d00ec253cb4440d8a0379cfe481fb07c0786e6
Kaushik Biswas <kbutsho@gmail.com>
2023-08-06 04:11:58 +0600

    update

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -6,10 +6,10 @@ import auth from '../../middleware/auth';
 import { USER_ROLE } from '../../../helpers/enums';
 const router = express.Router();
 
-router.post('/', validateRequest(CategoryValidation.categoryZodSchema), auth(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN), CategoryController.createCategory);
+router.post('/', validateRequest(CategoryValidation.categoryZodSchema), CategoryController.createCategory);
 router.get('/:id', CategoryController.getSingleCategory);
-router.patch('/:id', validateRequest(CategoryValidation.categoryZodSchema), auth(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN), CategoryController.updateCategory);
-router.delete('/:id', auth(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN), CategoryController.deleteCategory)
+router.patch('/:id', validateRequest(CategoryValidation.categoryZodSchema), CategoryController.updateCategory);
+router.delete('/:id', CategoryController.deleteCategory)
 router.get('/', CategoryController.getAllCategory)
 
 export const CategoryRoutes = router;
