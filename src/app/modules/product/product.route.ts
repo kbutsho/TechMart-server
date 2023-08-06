dc52923b9952cc08f62b8aaa2861552a3ff798e2
Kaushik Biswas <kbutsho@gmail.com>
2023-08-06 19:24:54 +0600

    update

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -2,15 +2,13 @@ import express from 'express';
 import validateRequest from '../../middleware/validateRequest';
 import { ProductValidation } from './product.validation';
 import { ProductController } from './product.controller';
-import auth from '../../middleware/auth';
-import { USER_ROLE } from '../../../helpers/enums';
 const router = express.Router();
 
 router.post('/', validateRequest(ProductValidation.productZodSchema), ProductController.createProduct);
-router.get('/seller-products', auth(USER_ROLE.SELLER), ProductController.getSellerAllProduct)
+router.get('/seller-products', ProductController.getSellerAllProduct)
 router.get('/:id', ProductController.getSingleProduct);
-router.patch('/:id', auth(USER_ROLE.SELLER, USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN), validateRequest(ProductValidation.productZodSchema), ProductController.updateProduct);
-router.delete('/:id', auth(USER_ROLE.SELLER, USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN), ProductController.deleteProduct)
+router.patch('/:id', validateRequest(ProductValidation.productZodSchema), ProductController.updateProduct);
+router.delete('/:id', ProductController.deleteProduct)
 router.get('/', ProductController.getAllProduct)
 
 
